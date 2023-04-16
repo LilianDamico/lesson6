029883ba5811762e592f8e25ffc56c43e4bfd495
Lilian Maria Damico da Fonseca <100726998+LilianDamico@users.noreply.github.com>
2023-04-16 09:14:06 -0300

    Add files via upload

---
+++ lesson6.java
@@ -0,0 +1,15 @@
+package curso_java;
+
+import javax.swing.*;
+
+public class lesson6 {
+    public static void main(String[] args){
+        double n1 = Double.parseDouble((JOptionPane.showInputDialog("Informe a primeira nota")));
+        double n2 = Double.parseDouble((JOptionPane.showInputDialog("Informe a segunda nota")));
+        double n3 = Double.parseDouble((JOptionPane.showInputDialog("Informe a terceira nota")));
+        double n4 = Double.parseDouble((JOptionPane.showInputDialog("Informe a quarta nota")));
+        double med = (n1 + n2 + n3 + n4) / 4;
+        JOptionPane.showMessageDialog(null, " A média do aluno foi "+med);
+
+    }
+}
